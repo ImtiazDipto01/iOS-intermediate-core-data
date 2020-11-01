a59245974e5864dd5cc2ea7b864de4308724759c
imtiaz <programmer.imtiaz@gmail.com>
2020-11-01 18:25:04 +0600

    Add- Initial project setup with UINavigationController done

--- Intermediate Core Data/SceneDelegate.swift
+++ Intermediate Core Data/SceneDelegate.swift
@@ -8,6 +8,13 @@
 
 import UIKit
 
+class  CustomNavigationController : UINavigationController {
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+        return .lightContent
+    }
+}
+
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
@@ -17,7 +24,20 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        //guard let _ = (scene as? UIWindowScene) else { return }
+        
+        
+        /*
+         * setting up the 'UINavigationController' with 'rootViewController'
+         */
+        if let windowScene = scene as? UIWindowScene {
+            window = UIWindow(windowScene: windowScene)
+            window?.makeKeyAndVisible()
+            
+            let companiesController = ViewController()
+            let navController = CustomNavigationController(rootViewController: companiesController)
+            window?.rootViewController = navController
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
--- Intermediate Core Data/ViewController.swift
+++ Intermediate Core Data/ViewController.swift
@@ -13,8 +13,57 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "plus"), style: .plain, target: self, action: #selector(handleAddCompany))
+        
+        let lightRed = UIColor.getColor(red: 247, green: 66, blue: 82)
+        configureNavigationBar(largeTitleColor: .white, backgoundColor: lightRed, tintColor: lightRed, title: "Companies", preferredLargeTitle: true)
     }
+    
+    
+    @objc func handleAddCompany() {
+        print("Something going to happen")
+    }
+    
+}
 
 
+extension UIViewController {
+    
+    func configureNavigationBar(largeTitleColor: UIColor, backgoundColor: UIColor, tintColor: UIColor, title: String, preferredLargeTitle: Bool) {
+        if #available(iOS 13.0, *) {
+            let navBarAppearance = UINavigationBarAppearance()
+            navBarAppearance.configureWithOpaqueBackground()
+            navBarAppearance.largeTitleTextAttributes = [.foregroundColor: largeTitleColor]
+            navBarAppearance.titleTextAttributes = [.foregroundColor: largeTitleColor]
+            navBarAppearance.backgroundColor = backgoundColor
+
+            navigationController?.navigationBar.standardAppearance = navBarAppearance
+            navigationController?.navigationBar.compactAppearance = navBarAppearance
+            navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+
+            navigationController?.navigationBar.prefersLargeTitles = preferredLargeTitle
+            navigationController?.navigationBar.isTranslucent = false
+            navigationController?.navigationBar.tintColor = tintColor
+            navigationItem.title = title
+
+        } else {
+            // Fallback on earlier versions
+            navigationController?.navigationBar.isTranslucent = false
+            navigationController?.navigationBar.barTintColor = tintColor
+            navigationController?.navigationBar.prefersLargeTitles = preferredLargeTitle
+            navigationItem.title = title
+        }
+    }
+}
+
+
+extension UIColor{
+    
+    static func getColor(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
 }
 
